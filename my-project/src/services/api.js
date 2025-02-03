6d9e2c1096618ecc897679d5d71618604a1fb9c3
Sheshanadaf <115085953+Sheshanadaf@users.noreply.github.com>
2025-02-04 04:11:55 +0530

    seconssd

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',  // Adjust if needed for production
+  baseURL: import.meta.env.VITE_API_URL,  // Adjust if needed for production
 });
 
 export const getLibraryData = async () => {
