db72c1170b0fda690b0d478d087c3a61ef1ef6e6
Sheshanadaf <115085953+Sheshanadaf@users.noreply.github.com>
2025-02-04 19:34:49 +0530

    second4hl commitddhhs

--- backend/server.js
+++ backend/server.js
@@ -7,22 +7,14 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 // API endpoint to get library data
-// Sample API endpoint to get library data with id, name, author, year
 app.get("/library", async (req, res) => {
   try {
-    // Static data to represent your library items
-    const items = [
-      { id: 1, name: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-      { id: 2, name: "1984", author: "George Orwell", year: 1949 },
-      { id: 3, name: "Moby Dick", author: "Herman Melville", year: 1851 }
-    ];
-
+    const items = await getItems();
     res.json(items);  // Return the data as JSON
   } catch (error) {
     res.status(500).json({ message: "Error fetching data", error });
   }
 });
-
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');  // ✅ ALB will check this response
